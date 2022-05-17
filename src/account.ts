8114ed50b38161ed1aa33ff9f4615ff40b6e0cdd
zzGHzz <zzGHzz@github>
2022-05-17 13:11:50 +0800

    add hdwallet config

--- src/account.ts
+++ src/account.ts
@@ -1,11 +1,17 @@
 import { HDNode } from 'thor-devkit'
 
-const words = 'denial kitchen pet squirrel other broom bar gas better priority spoil cross'
-const hdNode = HDNode.fromMnemonic(words.split(' '))
+const word = 'denial kitchen pet squirrel other broom bar gas better priority spoil cross'
+const hdNode = HDNode.fromMnemonic(word.split(' '))
 
 let defaultAccounts: string[] = []
 for (let i = 0; i < 10; i++) {
 	const priv = hdNode.derive(i).privateKey
 	defaultAccounts.push('0x' + priv?.toString('hex'))
 }
-export { defaultAccounts }
+
+const HDWalletConfig = {
+	mnemonic: word,
+	path: `m/44'/818'/0'/0`
+}
+
+export { defaultAccounts, HDWalletConfig }
